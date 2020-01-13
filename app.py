8e499c8dc8eb842f8e8091ff3a1accbd5ce81e2a
Ubuntu <ubuntu@ip-172-31-44-115.ec2.internal>
2020-01-13 12:29:47 +0000

    added default route decorator in app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask (__name__)
 
+mongo = PyMongo(app)
+
+
+
+
 @app.route('/')
-def :
+@app.route('/get_chapters')
+def get_chapters():
+    return render_template("chapters.html", chapters = mongo.db.chapters.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
