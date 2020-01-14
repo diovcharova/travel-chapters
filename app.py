0388973f273257223c71f820befcba09753d55f2
Ubuntu <ubuntu@ip-172-31-44-115.ec2.internal>
2020-01-14 12:25:25 +0000

    initial form for adding a new chapter added to addchapter.html

--- app.py
+++ app.py
@@ -12,15 +12,25 @@ mongo = PyMongo(app)
 
 
 
-
 @app.route('/')
 @app.route('/get_chapters')
 def get_chapters():
-    return render_template("chapters.html", chapters = mongo.db.chapters.find())
+    return render_template("chapters.html", chapters= mongo.db.chapters.find())
     
 @app.route('/add_chapter')
 def add_chapter():
-    return render_template("chapters.html", chapters = mongo.db.chapters.find())
+    return render_template("addchapter.html", countries = mongo.db.countries.find())
+
+@app.route('/insert_chapter', methods=['POST'])
+def insert_chapter():
+    chapters = mongo.db.chapters
+    chapters.insert_one(request.form.to_dict())
+    return redirect(url_for('get_chapters'))
+
+@app.route('/add_country')
+def add_country():
+    return render_template('addcountry.html')
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
